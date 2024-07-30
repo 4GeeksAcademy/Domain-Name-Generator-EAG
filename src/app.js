c0db84973dba4146c792473bada2de03fd85f189
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-07-30 22:09:59 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const paragraph = document.querySelector("#parrafo");
+console.log(paragraph);
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+const dominioName =
+  pronoun[Math.floor(Math.random() * pronoun.length)] +
+  adj[Math.floor(Math.random() * adj.length)] +
+  noun[Math.floor(Math.random() * noun.length)] +
+  ".com";
+
+paragraph.textContent = dominioName;
